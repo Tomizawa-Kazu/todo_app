722b3a4628287b6859b16d0989eeba5fbb0f9e3b
Tomizawa-Kazu <syuzen101492@gmail.com>
2022-11-21 21:11:59 +0900

    frontend 実装

--- frontend/src/components/TaskItem.tsx
+++ frontend/src/components/TaskItem.tsx
@@ -5,12 +5,18 @@ import { useAppDispatch } from '../app/hooks'
 import { setEditedTask } from '../slices/appSlice'
 import { useMutateTask } from '../hooks/useMutateTask'
 
-const TaskItemMemo: VFC<Task> = ({ id, title, description}) => {
+const TaskItemMemo: VFC<
+  Task & {
+    setId: React.Dispatch<React.SetStateAction<string>>
+  }
+> = ({ id, title, description, setId }) => {
   const dispatch = useAppDispatch();
   const { deleteTaskMutation } = useMutateTask();
   return (
     <li>
-      <span className="font-bold cursor-pointer-bold">{title}</span>
+      <span className="font-bold cursor-pointer" onClick={() => setId(id)}>
+        {title}
+      </span>
       <div className="flex float-right ml-20">
         <PencilAltIcon
           className="h-5 w-5 mx-1 text-blue-500 cursor-pointer"
